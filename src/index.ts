f04e5bcb24308395b9a1ee0e519ef97d4fdab1b3
Ron Perry <ronp001@gmail.com>
2019-01-15 23:32:12 +0200

    initial demonstration of problem

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export class DemoOfProblem {
+    constructor(
+        public argument_with_default = "default_value"
+    ) { }
+}
+
+export class NoProblemHere {
+    constructor(
+        argument_with_default = "default_value"
+    ) { }
+}
