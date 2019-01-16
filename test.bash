6e046cf32e8b24da60a936121b0a26d89e33d05b
Ron Perry <ronp001@gmail.com>
2019-01-16 23:41:20 +0200

    demonstration of problem and solution

--- test.bash
+++ test.bash
@@ -1,1 +1,11 @@
-echo "********** step 1:  running babel using "
+echo "********** step 1:  running babel with metro-react-native-babel-preset"
+npx babel --compact=false --config-file ./babelrc-only-metro-preset.jsonc src/* > output_preset.js
+cat output_preset.js
+
+echo "********** step 2:  running babel with transform-parameters AFTER typescript"
+npx babel --compact=false --config-file ./babelrc-with-transform-after-ts.jsonc src/* > output_transform_after_ts.js
+cat output_transform_after_ts.js
+
+echo "********** comparing output (should be identical)"
+diff output_preset.js output_transform_after_ts.js
+
